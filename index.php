87b0d9c4a405d47f26697d4ae65b48cb34cbe078
nick van oostrom <nick.v.oostrom@gmail.com>
2021-10-11 15:00:29 +0200

    paginas toegevoegd

--- index.php
+++ index.php
@@ -8,10 +8,18 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-<?php
-    include ('navbar.php');
-
-?>
+  <div class="container-fluid px-0">
+    <div class="row">
+      <div class="col-12">
+        <?php include("./pagina-start/navbar.php"); ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <?php include("./pagina-start/content.php"); ?>
+      </div>
+    </div>
+  </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
--- pagina-start/navbar.php
+++ pagina-start/navbar.php
@@ -7,25 +7,25 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="./index.php?content=home">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Agenda</a>
+          <a class="nav-link" href="./index.php?content=agenda">Agenda</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Praktische Informatie</a>
+          <a class="nav-link" href="./index.php?content=praktische-info">Praktische Informatie</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Nieuws</a>
+          <a class="nav-link" href="./index.php?content=nieuws">Nieuws</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+          <a class="nav-link" href="./index.php?content=contact">Contact</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Eng/Ned</a>
+          <a class="nav-link" href="./index.php?content=eng-nl">Eng/Ned</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
+          <a class="nav-link" href="./index.php?content=login">Login</a>
         </li>
       </ul>
     </div>
