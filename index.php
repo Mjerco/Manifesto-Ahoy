eb5664b33063190d3a6f2992c114ed50d13b7a8e
Mjerco <327236@student.mboutrecht.nl>
2021-10-11 15:02:57 +0200

    Mijn nieuwe files

--- index.php
+++ index.php
@@ -7,11 +7,24 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="./CSS/style.css" rel="stylesheet">
 
 <?php
     include ('navbar.php');
-
 ?>
+
+<div class="row" id="content">
+  <div class="col-12">
+    <?php
+      if ( isset($_GET["content"]) ){
+        include("./" . $_GET["content"] . ".php");
+      }
+      else {
+        include("./homepage.php");
+      }
+    ?>
+  </div>
+</div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
--- navbar.php
+++ navbar.php
@@ -1,31 +1,30 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <a class="navbar-brand" href="#">
+      <img src="./IMG/rotterdam-ahoy-logo.png" alt="Logo" width="30" height="24">
+    </a>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="index.php?content=homepage">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Agenda</a>
+          <a class="nav-link" href="index.php?content=agenda">Agenda</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Praktische Informatie</a>
+          <a class="nav-link" href="index.php?content=praktische-info">Praktische Informatie</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Nieuws</a>
+          <a class="nav-link" href="index.php?content=nieuws">Nieuws</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+          <a class="nav-link" href="index.php?content=contact">Contact</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Eng/Ned</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
+          <a class="nav-link" href="index.php?content=login">Login</a>
         </li>
       </ul>
     </div>
