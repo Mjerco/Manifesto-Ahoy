e2e5e35be5a35ec70defc24d7f6305ca3caf0478
Mjerco <327236@student.mboutrecht.nl>
2021-10-19 14:33:01 +0200

    Eerste Event in Agenda

---
+++ pagina/agenda.php
@@ -0,0 +1,24 @@
+<div class="container">
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-1">
+        <p class="date">December 01<p>
+        </div>
+        <div class="col-3">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="text-card">Winter Fair</p>
+                            <p class="text-card">Beurs</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-3"></div>
+    </div>
+</div> 
