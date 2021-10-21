b8688121e1cb4565861a0d2a519d2a65423a5ed6
Mjerco <327236@student.mboutrecht.nl>
2021-10-21 14:28:41 +0200

    Events toegevoegd in Agenda, Background kleur verandert

--- pagina/agenda.php
+++ pagina/agenda.php
@@ -1,12 +1,38 @@
 <div class="container">
     <div class="row">
         <div class="col-2"></div>
-        <div class="col-1">
-        <p class="date">December 01<p>
-        </div>
-        <div class="col-3">
+        <div class="col-4">
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
+                    <div class="col-md-4">
+                        <p class="date">01 December</p>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="text-card">Winter Fair</p>
+                            <p class="text-card">Beurs</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <p class="date">02 December</p>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="text-card">Winter Fair</p>
+                            <p class="text-card">Beurs</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <p class="date">03 December</p>
+                    </div>
                     <div class="col-md-8">
                         <div class="card-body">
                             <p class="text-card">Winter Fair</p>
