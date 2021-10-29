aa93815d50da6c3bfb2519a0225171d65d1f77bf
Mjerco <327236@student.mboutrecht.nl>
2021-10-29 19:13:19 +0200

    Agenda aangevuld

--- pagina/agenda.php
+++ pagina/agenda.php
@@ -1,5 +1,120 @@
 <div class="container">
     <div class="row">
+        <div class="col-2">
+            <P>Dec</p>
+            <p>2021</p>
+        </div>
+        <div class="col-4">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <p class="date">01 December</p>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="text-card">Winter Fair</p>
+                            <p class="text-card">Beurs</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <p class="date">02 December</p>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="text-card">Winter Fair</p>
+                            <p class="text-card">Beurs</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <p class="date">03 December</p>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="text-card">Winter Fair</p>
+                            <p class="text-card">Beurs</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="card bg-dark text-white">
+                <img src="./IMG/winter-fair.jpg" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Winterfair</h5>
+                    <p class="card-text">1 tot 5 December 2021</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-1"></div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-4">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <p class="date">04 December</p>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="text-card">Winter Fair</p>
+                            <p class="text-card">Beurs</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <p class="date">05 December</p>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="text-card">Winter Fair</p>
+                            <p class="text-card">Beurs</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <p class="date">07 December</p>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="text-card">Wooning Zesdaagse</p>
+                            <p class="text-card">18:00 - Sport</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="card bg-dark text-white">
+                <img src="./IMG/wooning-zesdaagse-ahoy.jpg" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Wooning Zesdaagse</h5>
+                    <p class="card-text">7 tot 12 December 2021</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-1"></div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
         <div class="col-2"></div>
         <div class="col-4">
             <div class="card mb-3" style="max-width: 540px;">
@@ -42,9 +157,15 @@
                 </div>
             </div>
         </div>
-        <div class="col-3">
-
+        <div class="col-5">
+            <div class="card bg-dark text-white">
+                <img src="./IMG/studiekeuzebeurs.jpg" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Studiekeuzebeurs West</h5>
+                    <p class="card-text">10 & 11 December</p>
+                </div>
+            </div>
         </div>
-        <div class="col-3"></div>
+        <div class="col-1"></div>
     </div>
 </div> 
